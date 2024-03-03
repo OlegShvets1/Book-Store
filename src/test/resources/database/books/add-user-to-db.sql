43dcdc947308a77c6a6010b0faf7633d46e2fbb3
Oleh Shvets <olehpost69@gmail.com>
2024-03-03 10:19:28 +0300

    added_tests_forSC_2

--- src/test/resources/database/books/add-user-to-db.sql
+++ src/test/resources/database/books/add-user-to-db.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (email, password, first_name, last_name, shipping_address, is_deleted)
-VALUES ('oleh@example.com', 'password', 'Oleh', 'S', '123 Street, City', false);
+INSERT INTO users (id, email, password, first_name, last_name, shipping_address, is_deleted)
+VALUES (1, 'oleh@example.com', 'password', 'Oleh', 'S', '123 Street, City', false);
